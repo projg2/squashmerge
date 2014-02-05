1c7eba82856691c285725ac13dc32ef316ae30d3
Michał Górny <mgorny@gentoo.org>
2014-02-05 10:04:52 +0100

    Initiate the codebase.

---
+++ src/squashmerge.c
@@ -0,0 +1,14 @@
+/**
+ * SquashFS delta merge tool
+ * (c) 2014 Michał Górny
+ * Released under the terms of the 2-clause BSD license
+ */
+
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
